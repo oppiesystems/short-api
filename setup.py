cb3882e2724563c5ff5fc1e1078a20b608f4a0c9
Pat Migliaccio <pat@patmigliaccio.com>
2019-10-11 10:39:00 -0400

    add setup.py with versioning 0.1.1

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+  name='short-api',
+  version='0.1.1',
+  description='Domain-specific content summarization API',
+  author='Pat Migliaccio',
+  author_email='pat@oppie.io',
+  url='',
+  license='GNU GPL'
+)
